4fdc82aa8840e143eb2fc86db7d56ef0353d952a
Isidora Aleksic <isidora123456666@gmail.com>
2023-11-29 19:42:02 +0100

    Adding account service and connecting to our server.

--- src/app/account/account-management/account-management.component.ts
+++ src/app/account/account-management/account-management.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from "../account.service";
+import { Account,Address, User } from "../model/model.module";
 
 @Component({
   selector: 'app-account-management',
@@ -7,11 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountManagementComponent implements OnInit {
   
-  constructor(){};
-  ngOnInit() {
-    
+  user: User | undefined;
+  account: Account | undefined;
+  address: Address | undefined;
+  constructor(private service: UserService) {
   }
+
+
+  ngOnInit(): void {
+    this.service.getUser(1).subscribe({
+      next: (data: User) => {
+        this.user = data;
+        // this.address = data.address;
+        // this.account = data.account;
+      },
+      error: (_) => {console.log("Greska!")}
+    })
+  }
+
   url: string|null|ArrayBuffer = '../../../assets/images/addpicture.png' 
+  
   onFileSelected(files: FileList | null) {
       if (files) {
           var reader = new FileReader()
