c8ab9443084f479ceb6797c8ed444dd24beba05d
VanjaJe <jevtic.sv11.2021@uns.ac.rs>
2024-01-22 22:12:06 +0100

    Implemented tests for account managment - update

--- src/app/account/account-management/account-management.component.spec.ts
+++ src/app/account/account-management/account-management.component.spec.ts
@@ -1,6 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountManagementComponent } from './account-management.component';
+import { By } from '@angular/platform-browser';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MaterialModule} from "../../infrastructure/material/material.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterTestingModule} from "@angular/router/testing";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {UserService} from "../account.service";
 
 describe('AccountManagementComponent', () => {
   let component: AccountManagementComponent;
@@ -8,8 +15,11 @@ describe('AccountManagementComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AccountManagementComponent]
+      declarations: [AccountManagementComponent],
+      providers: [UserService],
+      imports: [HttpClientTestingModule,MaterialModule,BrowserAnimationsModule, RouterTestingModule]
     });
+
     fixture = TestBed.createComponent(AccountManagementComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -17,5 +27,318 @@ describe('AccountManagementComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('all invalid fields', () => {
+    component.updateUserForm.controls['firstName'].setValue('');
+    component.updateUserForm.controls['lastName'].setValue('');
+    component.updateUserForm.controls['country'].setValue('');
+    component.updateUserForm.controls['city'].setValue('');
+    component.updateUserForm.controls['address'].setValue('');
+    component.updateUserForm.controls['phoneNumber'].setValue('');
+    component.updateUserForm.controls['username'].setValue('');
+    component.updateUserForm.controls['password'].setValue('');
+    component.updateUserForm.controls['confirmPassword'].setValue('');
+    component.updateUserForm.controls['phoneNumber'].setValue('');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('first name empty field', () => {
+    component.updateUserForm.controls['firstName'].setValue('');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimmitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('11238716');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+
+  it('first name length less then 3', () => {
+    component.updateUserForm.controls['firstName'].setValue('is');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimmitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('11238716');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('last name empty field', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimmitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('11238716');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+
+  it('last name length less then 3', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('al');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimmitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('11238716');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('country empty field', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimmitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('11238716');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('city empty field', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('');
+    component.updateUserForm.controls['address'].setValue('mise dimmitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('11238716');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+  it('address empty field', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('');
+    component.updateUserForm.controls['phoneNumber'].setValue('11238716');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+  it('phone number empty field', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('phone number invalid format field', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('dimitrijevica');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+
+  it('username empty field', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('dimitrijevica');
+    component.updateUserForm.controls['username'].setValue('');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+
+  it('username invalid format', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('dimitrijevica');
+    component.updateUserForm.controls['username'].setValue('aaa');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+
+  it('password empty field', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('dimitrijevica');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+
+
+  it('password length less then 6', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('dimitrijevica');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('confirm password empty field', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('dimitrijevica');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+
+
+  it('confirm password length less then 6', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('dimitrijevica');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('password and confirm password length is the same but not the same', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('dimitrijevica');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra456');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+  it('password and confirm password length less then 6 but are equals', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('dimitrijevica');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeTruthy();
+  });
+
+
+  it('valid', () => {
+    component.updateUserForm.controls['firstName'].setValue('isidora');
+    component.updateUserForm.controls['lastName'].setValue('aleksic');
+    component.updateUserForm.controls['country'].setValue('srbija');
+    component.updateUserForm.controls['city'].setValue('novi sad');
+    component.updateUserForm.controls['address'].setValue('mise dimitrijevica');
+    component.updateUserForm.controls['phoneNumber'].setValue('123456');
+    component.updateUserForm.controls['username'].setValue('aleksicisidora@yahoo.com');
+    component.updateUserForm.controls['password'].setValue('sifra123');
+    component.updateUserForm.controls['confirmPassword'].setValue('sifra123');
+
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css("#saveChanges"));
+    expect(button.nativeElement.disabled).toBeFalsy();
   });
 });
--- src/app/account/account.service.spec.ts
+++ src/app/account/account.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { UserService } from './account.service';
 import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import {mockUser1} from "../mocks/user.service.mock";
+import {Account, Address, Role, Status, User} from "./model/model.module";
 
 describe('UserService', () => {
   let service: UserService;
@@ -53,5 +54,49 @@ describe('UserService', () => {
   //
   //   req.flush(mockUser1);
   // });
+
+
+  it('should call update and the API should return the updated user', () => {
+    const mockAddress: Address = {
+      city: 'MockedCity',
+      country: 'MockedCountry',
+      address: 'MockedAddress',
+    };
+
+    const mockRole: Role = {
+      id: 1,
+      name: 'ROLE_GUEST',
+    };
+
+    const mockRoles: Role[] = [mockRole];
+
+    const mockAccount: Account = {
+      username: 'MockedUsername',
+      password: 'MockedPassword',
+      status: Status.ACTIVE,
+      roles: mockRoles,
+    };
+
+    const mockUser: User = {
+      firstName: 'MockedFirstName',
+      lastName: 'MockedLastName',
+      address: mockAddress,
+      phoneNumber: 'MockedPhoneNumber',
+      account: mockAccount,
+      picturePath: 'MockedPicturePath',
+      id: 5
+    };
+
+    service.update(mockUser).subscribe((data) => {
+      expect(data).toEqual(mockUser);
+    });
+
+    const req = httpController.expectOne({
+      method: 'PUT',
+      url: `${url}/users/${mockUser.id}`,
+    });
+
+    req.flush(mockUser);
+  });
 
 });
